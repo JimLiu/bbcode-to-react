692c0c065a76fd07e723edaafac63db5c2c23e99
Jim Liu <junminliu@gmail.com>
2016-11-01 01:46:14 -0500

    chore(release): adding 0.2.4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Parser from './parser';
 import Tag from './tag';
 
-export default new Parser();
+// export default new Parser();
 
-export {
-  Parser,
-  Tag
-};
+// export {
+//   Parser,
+//   Tag
+// };
+
+module.exports = new Parser();
+module.exports.Parser = Parser;
+module.exports.Tag = Tag;
