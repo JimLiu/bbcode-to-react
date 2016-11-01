b6633ed161c7a40c8945cce9afbddda78222d7d4
Jim Liu <junminliu@gmail.com>
2016-11-01 01:55:53 -0500

    chore(release): adding 0.2.5

--- tonic-example.js
+++ tonic-example.js
@@ -2,12 +2,43 @@ var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var bbcode = require('bbcode-to-react');
 
-class MyComponent extends React.Component {
-  render() {
-    console.log(bbcode.default);
-    return <p>{bbcode.default.toReact('[b]strong[/b]')}</p>;
+/* Code for customize tag
+class YoutubeTag extends bbcode.Tag {
+  toReact() {
+    // using this.getContent(true) to get it's inner raw text.
+    const attributes = {
+      src: this.getContent(true),
+      width: this.params.width || 420,
+      height: this.params.height || 315,
+    };
+    return (
+      <iframe
+        {...attributes}
+        frameBorder="0"
+        allowFullScreen
+      />
+    );
   }
 }
 
-// render: <p><strong>strong</strong></p>
+class BoldTag extends bbcode.Tag {
+  toReact() {
+    // using this.getComponents() to get children components.
+    return (
+      <b>{this.getComponents()}</b>
+    );
+  }
+}
+
+// [youtube width="400"]https://www.youtube.com/watch?v=AB6RjNeDII0[/youtube]
+bbcode.registerTag('youtube', YoutubeTag); // add new tag
+bbcode.registerTag('b', BoldTag); // replace exists tag
+*/
+
+class MyComponent extends React.Component {
+  render() {
+    return <p>{bbcode.toReact('[b]My Video: [/b][youtube width="400"]https://www.youtube.com/watch?v=AB6RjNeDII0[/youtube]')}</p>;
+  }
+}
+
 ReactDOMServer.renderToString(<MyComponent />);
